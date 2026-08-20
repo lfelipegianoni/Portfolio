c994c4b6a5ee37c12cd7a81c3300f931a8f8cfdb
luis <lfelipe2305@hotmail.com.br>
2026-08-20 20:15:51 -0300

    texts add

--- src/App.jsx
+++ src/App.jsx
@@ -11,16 +11,21 @@ function App() {
       <Header/>
       <main>
         <section className="home">
-          <h3>I`m a Softer Developer</h3>
+          <h5>I`M SOFTWARE ENGINEER</h5>
+          <p>Hi, I`m Luis Felipe <br/> I build  things for the web</p>
+          <p>I'm a Full-Stack Developer specializing in building <br/> exceptional digital experiences with modern technologies</p>
+          <a>linkedin</a>
+          <button>Dowload CV</button>
+          <h5>TECHNOLOGIES | WORK WITH</h5>
         </section>
         <section className="about">
-          <h3>About</h3>
+          <h5>About</h5>
         </section>
         <section className="skills">
-          <h3>My Skills</h3>
+          <h5>My Skills</h5>
         </section>
         <section className="project">
-          <h3>My best projects</h3>
+          <h5>My best projects</h5>
         </section>
       </main>
       <footer className="contact">
