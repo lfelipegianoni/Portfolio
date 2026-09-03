ee3f0a1729a45fb815d76008c522c82403b9288a
luis <lfelipe2305@hotmail.com.br>
2026-09-03 14:57:27 -0300

    changing the name of the pages folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { Header } from './pages/Header';
-import { Home } from './pages/Home';
-import { About } from './pages/About';
-import { Skills } from './pages/Skills';
-import { Project } from './pages/Project';
-import { Footer } from './pages/Footer';
+import { Header } from './sections/Header';
+import { Home } from './sections/Home';
+import { About } from './sections/About';
+import { Skills } from './sections/Skills';
+import { Project } from './sections/Project';
+import { Footer } from './sections/Footer';
 
 function App() {
   return (
